4c83af8ab7f9cbb8283a34e7ea53fb0247625aaf
Tom Maurer <tom.maurer@vizworx.com>
2020-02-26 16:28:39 -0700

    Basic rotator script
    Vscode is broken for this project

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,26 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// public enum Direction
-//     {
-//         left,
-//         right
-//     }
+public enum Direction
+    {
+        left,
+        right
+    }
 
 public class Rotator : MonoBehaviour
 {
 
     public float speed;
-    // public Direction direction;
+    public Direction direction;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 rotate = direction == Direction.left ? new Vector3(0,speed, 0) : new Vector3(0, -speed, 0);
+        gameObject.transform.Rotate(rotate);
            
     }
 }
